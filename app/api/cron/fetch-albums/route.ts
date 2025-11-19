2f1888383677fa55f4a8d267fd5dd06720e7c1aa
sahil-kaler <119391867+sahil-kaler@users.noreply.github.com>
2025-11-19 20:47:10 +0530

    fix

--- app/api/cron/fetch-albums/route.ts
+++ app/api/cron/fetch-albums/route.ts
@@ -1,3 +1,7 @@
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
+
 import { NextResponse } from "next/server"
 import { getNewReleases } from "@/lib/spotify"
 import { prisma } from "@/lib/prisma"
